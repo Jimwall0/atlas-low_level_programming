23120932ed85bcaa7ea14badf47c5a35552eb6ba
Tsimmuaj <tyang4204@gmail.com>
2024-03-07 17:22:04 -0600

    Files uploaded

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_listint - print the elements in the struct
+ * @h: struct that needs to be printed
+ * Return: the struct again?
+*/
+size_t print_listint(const listint_t *h)
+{
+	printf("%d\n", p->h);
+	return (h);
+}
