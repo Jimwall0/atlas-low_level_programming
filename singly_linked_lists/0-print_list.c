662a7a75d7b8afb269e0476afe20fbafe7b40b6f
Tsimmuaj <tyang4204@gmail.com>
2024-03-07 13:22:27 -0600

    Betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,43 +1,43 @@
 #include "lists.h"
-/*
- * size_t print_list - prints the the nodes in a singly linked list
- * 
+/**
+ * print_list - prints the the nodes in a singly linked list
+ * @h: list provided to print
+ * Return: number of elements
  */
 size_t print_list(const list_t *h)
 {
-    size_t numb = 0;
-    char *string;
-    unsigned int number = 0;
-    const list_t *node = h;
-    if (node == NULL)
-    {
-        return (numb);
-    }
-    while (node->next != NULL)
-    {
-        string = node->str;
-        number = node->len;
-        if (string == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%d] %s\n", number, string);
-        }
-        node = node->next;
-        numb++;
-    }
-    string = node->str;
-    number = node->len;
-    if (string == NULL)
-    {
-        printf("[0] (nil)\n");
-    }
-    else
-    {
-        printf("[%d] %s\n", number, string);
-    }
-    numb++;
-    return (numb);
+	size_t numb = 0;
+	char *string;
+	unsigned int number = 0;
+	const list_t *node = h;
+
+	if (node == NULL)
+		return (numb);
+	while (node->next != NULL)
+	{
+		string = node->str;
+		number = node->len;
+		if (string == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", number, string);
+		}
+		node = node->next;
+		numb++;
+	}
+	string = node->str;
+	number = node->len;
+	if (string == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	else
+	{
+		printf("[%d] %s\n", number, string);
+	}
+	numb++;
+	return (numb);
 }
