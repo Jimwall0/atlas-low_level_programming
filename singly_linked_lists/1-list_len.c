e487241a0c23064f78b51d7c58ca2caf4d8817ae
Tsimmuaj <tyang4204@gmail.com>
2024-03-07 13:53:30 -0600

    Added NULL check

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,6 +8,10 @@ size_t list_len(const list_t *h)
 	const list_t *node = h;
 	size_t number = 0;
 
+	if (node == NULL)
+	{
+		return (0);
+	}
 	while (node->next != NULL)
 	{
 		if (node->str != NULL)
