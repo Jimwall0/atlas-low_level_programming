1d916b91ae5e2e802d0b2c98fe3b4656c557c434
Tsimmuaj <tyang4204@gmail.com>
2024-03-08 09:38:52 -0600

    Final Check

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees the whole lists?
+ * @head: the start of the list.
+*/
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+	{
+
+	}
+	else
+	{
+	while ((*head)->next != NULL)
+	{
+		free(*head);
+		*head = (*head)->next;
+	}
+	free(*head);
+	}
+    *head = NULL;
+}
