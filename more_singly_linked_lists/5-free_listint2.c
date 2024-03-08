bba66a72a3c5b9339642d7faf4bda01f746516a8
Tsimmuaj <tyang4204@gmail.com>
2024-03-08 09:42:21 -0600

    added second check

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -5,10 +5,14 @@
 */
 void free_listint2(listint_t **head)
 {
-	if (**head == NULL)
+	if (*head == NULL)
 	{
 
 	}
+    else if (head == NULL)
+    {
+
+    }
 	else
 	{
 	while ((*head)->next != NULL)
