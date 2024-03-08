27d24cfd89897b8b9ae16a4629dd4d9d6a8773e3
Tsimmuaj <tyang4204@gmail.com>
2024-03-08 10:28:56 -0600

    Changed format

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -5,16 +5,16 @@
 */
 void free_listint2(listint_t **head)
 {
-    listint_t *temp;
+	listint_t *temp;
 
-    if (head != NULL)
-    {
-        while (*head != NULL)
-        {
-            temp = *head;
-            *head = (*head)->next;
-            free(temp);
-        }
-        *head = NULL;
-    }
+	if (head != NULL)
+	{
+		while (*head != NULL)
+		{
+			temp = *head;
+			*head = (*head)->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
