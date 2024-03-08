7e48af39149a3679421e99aedef5eb3067809709
Tsimmuaj <tyang4204@gmail.com>
2024-03-08 12:53:57 -0600

    Check code

---
+++ more_singly_linked_lists/7-get_nodeint_at_index.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - Grabs the integer element inside
+ * a struct list
+ * @head: pointer to the head node
+ * @index: what specific node to stop at
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+    listint_t *temp;
+    unsigned int number = 0;
+
+    if (head == NULL)
+    {
+        return (NULL);
+    }
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        if (number == index)
+        return (temp);
+        number++;
+    }
+    return (NULL);
+}
