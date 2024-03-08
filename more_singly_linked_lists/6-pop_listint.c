fd27d2c83fe0036a88d50688a334dcd93f5b9fca
Tsimmuaj Yang <tyang4204@gmail.com>
2024-03-08 12:28:57 -0600

    finished

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * pop_listint - removes a struct from the front
+ * @head: list of structs
+ * Return: true or false?
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return ((*head)->n);
+}
