1930774261461616b4949f04d4ef43757b86d441
Tsimmuaj <tyang4204@gmail.com>
2024-03-08 09:16:28 -0600

    Finalize

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,20 +1,16 @@
-#include "lists.c"
+#include "lists.h"
 /**
  * free_listint - frees list after use
  * @head: what lists we are freeing
 */
 void free_listint(listint_t *head)
 {
-    if (head == NULL)
-    {
+	if (head == NULL)
+	{
 
-    }
-    else
-    {
-        while (head->next != NULL)
-        {
-            free(head);
-        }
-        free(head);
-    }
+	}
+	else
+	{
+		free(head);
+	}
 }
