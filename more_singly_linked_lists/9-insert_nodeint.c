55e627e8b00f9a381fa04cb29ec611c3cbe46dae
Tsimmuaj <tyang4204@gmail.com>
2024-03-09 10:37:52 -0600

    Initial test

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - insert a new node at n
+ * @head: list of structs
+ * @idx: the index of the list
+ * @n: an integer?
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node = NULL, *start = *head, *temp;
+	unsigned int number = 0;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->n = n;
+	if (head != NULL)
+	{
+		while (*head != NULL)
+		{
+			if (idx == number)
+			{
+				temp = (*head)->next;
+				(*head)->next = node;
+				*head = temp;
+				node->next = *head;
+				*head = start;
+				return (*head);
+			}
+			(*head) = (*head)->next;
+			number++;
+		}
+	}
+	return (NULL);
+}
