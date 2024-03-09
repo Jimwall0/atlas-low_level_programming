fee333610a6973e1abc9be72182990d722c217ca
Tsimmuaj <tyang4204@gmail.com>
2024-03-09 11:12:58 -0600

    second try

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (*head != NULL)
 		{
-			if (idx == number + 1)
+			if (idx == number)
 			{
 				temp = (*head)->next;
 				(*head)->next = node;
