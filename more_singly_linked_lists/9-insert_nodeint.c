3936a6324b2f83546317d5a7510887bedfec5c3c
Tsimmuaj <tyang4204@gmail.com>
2024-03-09 10:48:18 -0600

    changed index value

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node = NULL, *start = *head, *temp;
-	unsigned int number = 1;
+	unsigned int number = 0;
 
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
