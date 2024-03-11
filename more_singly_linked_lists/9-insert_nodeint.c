00850f673831c486d18654bd6948c958d9889027
Tsimmuaj <tyang4204@gmail.com>
2024-03-11 11:55:08 -0500

    changed numbers

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -8,8 +8,8 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node = NULL, *beforenode, *movingnode;
-	unsigned int beforenode_index = 1;
+	listint_t *node = NULL, *temp, *temp2;
+	unsigned int index = 1;
 
 	if (head == NULL)
 	{
@@ -22,23 +22,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	node->n = n;
-	if (idx > 1)
+	if (*head != NULL)
 	{
-		beforenode = *head;
-		while (beforenode_index < idx - 1)
+		if (idx < 2)
 		{
-			beforenode = beforenode->next;
-			beforenode_index++;
+			node->next = *head;
+			*head = node;
 		}
-		movingnode = beforenode->next;
-		beforenode->next = node;
-		node->next = movingnode;
-		return (*head);
-	}else
-	{
-		node->next = *head;
-		*head = node;
-		return (*head);
+		else
+		{
+			temp = *head;
+			while (index < idx)
+			{
+				temp = temp->next;
+				index++;
+			}
+			temp2 = temp;
+			temp = temp->next;
+			temp2->next = node;
+			node->next = temp;
+		}
 	}
-	return (NULL);
+	else 
+		return (NULL);
+	return (*head);
 }
