33ac37db2a97c4db7a32218b7232ffcca04e084b
Tsimmuaj <tyang4204@gmail.com>
2024-03-11 13:43:44 -0500

    Try again

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	/*checks for index and prevents going outside list*/
 	temp = *head;
-	for (loop = 0; loop < idx && temp->next != NULL; loop++)
+	for (loop = 0; loop < idx - 1 && temp->next != NULL; loop++)
 	{
 		temp = temp->next;
 	}
