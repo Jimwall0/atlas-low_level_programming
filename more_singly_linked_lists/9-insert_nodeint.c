f7bd7f37a2a56730bec1d774797916f5082e9901
Tsimmuaj <tyang4204@gmail.com>
2024-03-11 13:00:20 -0500

    check

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -8,13 +8,13 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node = NULL, *temp, *temp2, *check = *head;
-	unsigned int index = 1, max = 1;
+	listint_t *node = NULL, *temp;
+	unsigned int loop = 0;
 
+	/*checks the head if it exists*/
 	if (head == NULL)
-	{
 		return (NULL);
-	}
+	/*makes a new node*/
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
 	{
@@ -22,40 +22,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	node->n = n;
-	while (check != NULL)
+	/*adds node to front*/
+	if (*head == NULL || idx == 0)
 	{
-		check = check->next;
-		max++;
+		node->next = *head;
+		*head = node;
+		return (*head);
 	}
-	if (idx > max)
-		return (NULL);
-	if (*head != NULL)
+	temp = *head;
+	for (loop = 0; loop < idx - 1 && temp->next != NULL; loop++)
 	{
-		if (idx < 2)
-		{
-			node->next = *head;
-			*head = node;
-		}
-		else
-		{
-			temp = *head;
-			while (index < idx)
-			{
-				temp = temp->next;
-				index++;
-			}
-			if (temp->next == NULL)
-			{
-				temp->next = node;
-				return (*head);
-			}
-			temp2 = temp;
-			temp = temp->next;
-			temp2->next = node;
-			node->next = temp;
-		}
+		temp = temp->next;
 	}
-	else 
+	if (idx > 0 && temp->next == NULL)
+	{
+		free(node);
 		return (NULL);
+	}
+	node->next = temp->next;
+	temp->next = node;
 	return (*head);
 }
