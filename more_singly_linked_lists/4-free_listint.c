0d362674d5882cad16b16e83cd89608c21795d57
Tsimmuaj <tyang4204@gmail.com>
2024-07-06 09:26:35 -0500

    added free list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_listint - frees a list
+ * @head: the start of a list
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp = NULL;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
