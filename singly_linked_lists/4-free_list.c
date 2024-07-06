27a50b74b3433358506574b2dfae3b33df53849c
Tsimmuaj <tyang4204@gmail.com>
2024-07-05 23:06:57 -0500

    free function

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: the head of a list
+*/
+void free_list(list_t *head)
+{
+	list_t *temp = NULL;
+	while (head != NULL);
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
