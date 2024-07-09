0e0e1a8a555f5f1512f696f50603d11dbdecf518
Tsimmuaj <tyang4204@gmail.com>
2024-07-08 21:23:31 -0500

    save

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: name of the text file
+ * @letters: the character count of the file
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char buffer[letters];
+	int total, file, letter;
+
+	file = open(filename, )
+}
