e07222e6a111d9754cb513e630ce9e11e5f29edf
Tsimmuaj <tyang4204@gmail.com>
2024-07-08 21:45:40 -0500

    added checks

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,9 +9,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer = NULL;
 	int file, letter;
 
+	if (filename == NULL || letters == 0)
+		return (0);
 	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
 	file = open(filename, O_RDONLY);
+	if (!file)
+	{
+		free(buffer);
+		return (0);
+	}
 	letter = read(file, buffer, letters);
+	if (!letter)
+	{
+		free(buffer);
+		return (0);
+	}
 	write(1, buffer, letter);
 	close(file);
 	return (letter);
