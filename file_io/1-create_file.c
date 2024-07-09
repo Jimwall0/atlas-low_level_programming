84456afe85da36a0082422a7985e952ed1db80b0
Tsimmuaj <tyang4204@gmail.com>
2024-07-08 23:03:16 -0500

    removed extra function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,16 +13,18 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file < 0)
 		return (0);
-	buffer = malloc(sizeof(char) * strlen(text_content));
+	while (text_content[loop] != '\0')
+		loop++;
+	buffer = malloc(sizeof(char) * loop + 1);
 	if (!buffer)
 	{
 		close(file);
 		free(buffer);
 		return (0);
 	}
-	for (; text_content[loop] != '\0'; loop++)
+	for (loop = 0; text_content[loop] != '\0'; loop++)
 		buffer[loop] = text_content[loop];
-	characters = write(file, buffer, strlen(text_content) + 1);
+	characters = write(file, buffer, loop + 1);
 	close(file);
 	free(buffer);
 	if (characters < 0)
