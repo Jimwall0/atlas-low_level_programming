134deef215a7dea5c7e20560f4d7a59d6e95ae6e
Tsimmuaj <tyang4204@gmail.com>
2024-07-08 22:56:59 -0500

    added text to buffer

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,37 +1,28 @@
 #include "main.h"
 /**
- * 
+ * create_file = makes a new text file
+ * @filename: name of the file
+ * @text_content: content of file
+ * Return: total characters or 0 on false
 */
 int create_file(const char *filename, char *text_content)
 {
 	char *buffer = NULL;
-	int characters, check, file, text;
+	int characters, file;
 
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file < 0)
-	{
-		close(file);
 		return (0);
-	}
-	text = open(text_content, O_RDWR);
-	if (text < 0)
-	{
-		close(file);
-		close(text);
-		return (0);
-	}
 	buffer = malloc(sizeof(char) * strlen(text_content));
 	if (!buffer)
 	{
 		close(file);
-		close(text);
 		free(buffer);
 		return (0);
 	}
-	check = read(text, buffer, strlen(text_content));
-	characters = write(file, buffer, check);
+	buffer = strdup(text_content);
+	characters = write(file, buffer, strlen(text_content) + 1);
 	close(file);
-	close(text);
 	free(buffer);
 	if (characters < 0)
 		return (0);
