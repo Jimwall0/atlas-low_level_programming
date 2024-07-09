58424c1e8c171a0f60686edcaa1fd957ae1c0995
Tsimmuaj <tyang4204@gmail.com>
2024-07-08 20:46:54 -0500

    added flip bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * flip_bits - returns the number of bits you need to toggle
+ * @n: starting number
+ * @m: converted number
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int bit = 1, result, total = 0;
+
+	result = n ^ m;
+	while (bit <= result)
+	{
+		if ((bit & result) == bit)
+			total++;
+		bit = bit << 1;
+	}
+	return (total);
+}
