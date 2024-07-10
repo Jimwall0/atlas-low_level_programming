7c6668af34a9a9da18e8fac06f8a51d1b975d130
Tsimmuaj <tyang4204@gmail.com>
2024-07-09 21:01:06 -0500

    return succes on empty text content

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,8 +9,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int characters, file, loop = 0;
 
-	if ((filename == NULL) | (text_content == NULL))
+	if ((filename == NULL))
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 	file = open(filename, O_TRUNC | O_WRONLY | O_CREAT, 0600);
 	if (file == -1)
 		return (-1);
