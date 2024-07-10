58ddaa78059f227155e3e9f9a4bb78c8f46d3ae0
Tsimmuaj <tyang4204@gmail.com>
2024-07-09 21:01:52 -0500

    betty check

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int characters, file, loop = 0;
 
-	if ((filename == NULL))
+	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
