84dde50db7a7b328fb3e90e967367503d554ce27
Tsimmuaj <tyang4204@gmail.com>
2024-07-09 20:30:37 -0500

    changed back to read and write permission

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if ((filename == NULL) | (text_content == NULL))
 		return (-1);
-	file = open(filename, O_RDONLY | O_TRUNC | O_CREAT, 0600);
+	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (file == -1)
 		return (-1);
 	while (text_content[loop] != '\0')
