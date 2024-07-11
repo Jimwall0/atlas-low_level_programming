fd0f9ecd3b4fc4e46e2069af8e03703965b0d1b1
Tsimmuaj <tyang4204@gmail.com>
2024-07-11 14:00:19 -0500

    added free list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a linked list
+ * @head: start of the list
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = NULL;
+	while (!head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
