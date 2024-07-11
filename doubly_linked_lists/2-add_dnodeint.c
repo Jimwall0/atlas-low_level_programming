0d08d71df5445280defd69564f0f8449b4011ceb
Tsimmuaj <tyang4204@gmail.com>
2024-07-11 11:35:55 -0500

    added NULL checks

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,7 +9,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t (*new) = NULL;
 
+	if (!head)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->n = n;
 	if (!(*head))
 	{
