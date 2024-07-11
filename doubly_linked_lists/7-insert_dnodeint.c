a692c710d2e4d279cbfd6c8b8001395a9ddf8c6b
Tsimmuaj <tyang4204@gmail.com>
2024-07-11 15:28:29 -0500

    more h issues

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if ((temp)->next == NULL)
 		return (add_dnodeint_end(h, n));
-	else if (idx == 0 || (*h) == NULL)
+	else if (idx == 0 || temp == NULL)
 		return (add_dnodeint(h, n));
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
