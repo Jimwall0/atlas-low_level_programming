8b7fa1a71465bf6bbee7315ddfd342b814dacc93
Tsimmuaj <tyang4204@gmail.com>
2024-07-11 14:07:15 -0500

    added index node function

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - grabs node at an index
+ * @head: the start of the list
+ * @index: the posistion of the node to return
+ * Return: the node at index
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int number = 0;
+
+	for (; number < index; number++)
+		head = head->next;
+	return (head);
+}
