6ddf00b72a432f52d3fa03803a11dbb4fb5853e9
Tsimmuaj <tyang4204@gmail.com>
2024-07-15 23:07:07 -0500

    changed back to add in front

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->value = strdup(value);
 	index = hash_djb2(string) % ht->size;
 	if (ht->array[index] != NULL)
-		free(ht->array[index]);
+		new->next = ht->array[index];
 	ht->array[index] = new;
 	free(string);
 	return (1);
