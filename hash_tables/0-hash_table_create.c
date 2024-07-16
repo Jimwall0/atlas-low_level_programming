d80204f08eab22877d933fb058f536c652893dd9
Tsimmuaj <tyang4204@gmail.com>
2024-07-15 21:09:54 -0500

    fixed missing closing bracket

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * @Return: the array
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+	return (table);
+}
