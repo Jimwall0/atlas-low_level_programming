2d8ff0d0707c81e24b5d81093726a2dd8b40a00c
Tsimmuaj <tyang4204@gmail.com>
2024-07-15 22:30:30 -0500

    fixed char to unsigned char

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to a table
+ * @ht: the table
+ * @key: the id
+ * @value: the number of the id
+ * Return: 1 on success an 0 on fail
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new = NULL;
+	long unsigned int index = 0;
+	const unsigned char *string = _strdup(key);
+
+	if (ht == NULL)
+		return (0);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->key = strdup(key);
+	new->value = strdup(value);
+	index = hash_djb2(string) % ht->size;
+	return (1);
+}
+
+/**
+ * _strdup - copies a char to unsigned char
+ * @string: copy
+ * Return: the string
+*/
+unsigned char *_strdup(const char *string)
+{
+	int number = 0;
+	unsigned char *letter;
+
+	while (string[number] != '\0')
+	{
+		letter[number] = string[number];
+		number++;
+	}
+	return (letter);
+}
