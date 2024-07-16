c53c362d13b3583d2aebb3ed7341bde4873abc33
Tsimmuaj <tyang4204@gmail.com>
2024-07-15 23:59:39 -0500

    save

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -6,21 +6,34 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int number = 0, count = 0, number2 = 0;
+	hash_node_t *temp = NULL;
 
 	printf("{");
 	for (; number < ht->size; number++)
 	{
 		if (ht->array[number] != NULL)
-			count++;
+		{
+			temp = ht->array[number];
+			while (temp != NULL)
+			{
+				temp = temp->next;
+				count++;
+			}
+		}
 	}
 	for (number = 0; number < ht->size; number++)
 	{
 		if (ht->array[number] != NULL)
 		{
-			printf("'%s': '%s'", ht->array[number]->key, ht->array[number]->value);
-			if (number2 < count - 1)
-				printf(", ");
-			number2++;
+			temp = ht->array[number];
+			while (temp != NULL)
+			{
+				printf("'%s': '%s'", temp->key, temp->value);
+				temp = temp->next;
+				if (number2 < count - 1)
+					printf(", ");
+				number2++;
+			}
 		}
 	}
 	printf("}\n");
