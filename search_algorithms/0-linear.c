ba06a949106c3c9f171a1deee6cf0d772ecd70a0
Tsimmuaj <tyang4204@gmail.com>
2024-08-13 11:39:07 -0500

    finished program

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -9,12 +9,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t loop = 0;
 
+	if (array == NULL)
+		return (-1);
 	for (loop = 0; loop < size; loop++)
 	{
-		printf("Value checked at index [%i]\n", loop);
+		printf("Value checked at index [%li]\n", loop);
 		if (array[loop] == value)
-		{
-			
-		}
+			return (loop);
 	}
+	return (-1);
 }
